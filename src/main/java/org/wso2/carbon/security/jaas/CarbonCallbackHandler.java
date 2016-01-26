e73b0210ba80cbfb8a1df9b42ac0c108baf790da
thanujalk <jatljayasinghe@gmail.com>
2016-01-26 16:08:14 +0530

    Adding carbon-security

---
+++ src/main/java/org/wso2/carbon/security/jaas/CarbonCallbackHandler.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.security.jaas;
+
+import io.netty.handler.codec.http.HttpRequest;
+
+import java.io.IOException;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+/**
+ *
+ */
+public class CarbonCallbackHandler implements CallbackHandler {
+
+    private HttpRequest httpRequest;
+
+    public CarbonCallbackHandler(HttpRequest httpRequest) {
+
+        this.httpRequest = httpRequest;
+    }
+
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+        if (callbacks != null) {
+            for (Callback callback : callbacks) {
+                if (callback instanceof CarbonCallback) {
+                    ((CarbonCallback) callback).setHttpRequest(this.httpRequest);
+                }
+            }
+        }
+    }
+}
